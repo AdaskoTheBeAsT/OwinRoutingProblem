98f6f66e2b2143fcfcef0f7e07b70b8a6e8907a7
Adam Pluciński <adaskothebeast@gmail.com>
2016-11-20 16:50:01 +0100

    2016-11-20 16:49 added frontend files to project, added gulp dev configuration and gulp cleanup for files generated from ts in visual studio

---
+++ OwinRoutingProblem/gulp/dev.js
@@ -0,0 +1,54 @@
+﻿'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var conf = require('./conf');
+
+var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
+});
+
+gulp.task('htmlDev', ['inject', 'partials'], function () {
+    var partialsInjectFile = gulp.src(path.join(conf.paths.tmp, '/partials/templateCacheHtml.js'), { read: false });
+    var partialsInjectOptions = {
+        starttag: '<!-- inject:partials -->',
+        ignorePath: path.join(conf.paths.tmp, '/partials'),
+        addRootSlash: false
+    };
+
+    var htmlFilter = $.filter('*.html', { restore: true, dot: true });
+    var jsFilter = $.filter('**/*.js', { restore: true, dot: true });
+    var cssFilter = $.filter('**/*.css', { restore: true, dot: true });
+
+    return gulp.src(path.join(conf.paths.tmp, '/serve/*.html'))
+      .pipe($.inject(partialsInjectFile, partialsInjectOptions))
+      .pipe($.useref())
+      .pipe(jsFilter)
+      .pipe($.sourcemaps.init())
+      //.pipe($.uglify({ preserveComments: $.uglifySaveLicense })).on('error', conf.errorHandler('Uglify'))
+      .pipe($.rev())
+      .pipe($.sourcemaps.write('maps'))
+      .pipe(jsFilter.restore)
+      .pipe(cssFilter)
+      // .pipe($.sourcemaps.init())
+      .pipe($.replace('../../bower_components/bootstrap-sass/assets/fonts/bootstrap/', '../fonts/'))
+      .pipe($.replace('../../bower_components/font-awesome/fonts/', '../fonts/'))
+      .pipe($.replace('../../bower_components/world-flags-sprite/images/', '../images/'))
+      //.pipe($.cssnano())
+      .pipe($.rev())
+      // .pipe($.sourcemaps.write('maps'))
+      .pipe(cssFilter.restore)
+      .pipe($.revReplace())
+      .pipe(htmlFilter)
+      /*.pipe($.htmlmin({
+          removeEmptyAttributes: true,
+          removeAttributeQuotes: true,
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true
+      }))*/
+      .pipe(htmlFilter.restore)
+      .pipe(gulp.dest(path.join(conf.paths.dist, '/')))
+      .pipe($.size({ title: path.join(conf.paths.dist, '/'), showFiles: true }));
+});
+
+gulp.task('dev', ['htmlDev', 'images', 'fonts', 'other']);
---
+++ OwinRoutingProblem/gulp/vsts.js
@@ -0,0 +1,49 @@
+﻿'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+var glob = require('glob');
+
+var tsjsfilter = function (file) {
+    return file.replace(/.ts$/, '.js');
+};
+
+gulp.task('cleanvsjs', function () {
+
+    return glob('./src/app/**/*.ts', function (err, files) {
+
+        var tsjsfiles = files.map(tsjsfilter);
+
+        tsjsfiles.forEach(function(tsjsfile) {
+            try {
+                del.sync(tsjsfile);
+            }
+            catch(err) {
+            }
+        });
+    });
+
+});
+
+var tsjsmapfilter = function (file) {
+    return file.replace(/.ts$/, '.js.map');
+};
+
+gulp.task('cleanvsjsMap', function () {
+
+    return glob('./src/app/**/*.ts', function (err, files) {
+        var tsjsmapfiles = files.map(tsjsmapfilter);
+        tsjsmapfiles.forEach(function(tsjsmapfile) {
+            try {
+                del.sync(tsjsmapfile);
+            }
+            catch (err) {
+            }
+        });
+    });
+
+});
+
+gulp.task('cleanvs', ['cleanvsjsMap', 'cleanvsjs'], function () {
+
+});
